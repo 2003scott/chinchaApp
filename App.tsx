28b397f5a49e348f4d51ba95e58ceea49362d699
Ckott <diegoangeles130703@gmail.com>
2025-02-16 18:34:56 -0500

    Agregar navegación y pantalla de inicio; implementar SplashScreen y configurar rutas

--- App.tsx
+++ App.tsx
@@ -5,26 +5,50 @@
  * @format
  */
 
-import React from 'react';
-import {
-    SafeAreaView,
-    ScrollView,
-    View,
-} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import { SplashScreen } from './src/components/animations/splashScreen';
+import { HomeScreen } from './src/screens';
 
+const Stack = createNativeStackNavigator()
 
 function App(): React.JSX.Element {
 
 
+    const [showSplash, setShowSplash] = useState(true)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowSplash(false)
+        }, 2000)
+
+        return () => clearTimeout(timer)
+    }, [])
+
     return (
-        <SafeAreaView>
-            <ScrollView
-                contentInsetAdjustmentBehavior="automatic">
-                <View>
-                    HOLA
-                </View>
-            </ScrollView>
-        </SafeAreaView>
+        <NavigationContainer>
+            {showSplash ? (
+                <SplashScreen />
+            ) : (
+                <>
+                    <Stack.Navigator initialRouteName="Home">
+                        <Stack.Screen name="Home" component={HomeScreen}
+                            options={{
+                                headerShown: false
+                            }} />
+                        <Stack.Screen name="DetailScreen" component={HomeScreen}
+                            options={{
+                                headerStyle: {
+                                    backgroundColor: "#ffffff",
+                                },
+                                headerTintColor: "#514F4F",
+                                headerTitle: ""
+                            }} />
+                    </Stack.Navigator>
+                </>
+            )}
+        </NavigationContainer>
     );
 }
 
